c8e1c9da46aba1aa83d65356dd12314fc9f4021b
do-ttery <do252425@gmail.com>
2026-07-09 21:44:06 +0900

    chore: project_v2 Vite+React 개발 환경 구성

---
+++ project_v2/src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import ProjectIntro from "../ProjectIntro.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ProjectIntro />
+  </React.StrictMode>
+);
